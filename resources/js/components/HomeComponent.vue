0224a146b5d1e0c0ddc2d6a663401f4dfb47d365
Luiz F. Lima <lf.system@outlook.com>
2021-10-19 20:55:34 -0300

    Commit init

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container-fluid p-0">
+    
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <h1>Card Home</h1>
+          </div>
+
+          <div class="card-body">
+            {{ example }}
+            <i class="fas fa-pencil-alt"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: ["data"],
+
+  data() {
+    return {
+      example: "Home Component",
+    };
+  },
+  mounted() {},
+  methods: {
+    acao() {},
+  },
+  beforeMount() {},
+};
+</script>
